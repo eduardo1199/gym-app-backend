1e1326f252501a5c41efabd891ef13d7fd794c1b
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-06-11 12:26:22 -0300

    feat: comment line

--- src/http/Routes.ts
+++ src/http/Routes.ts
@@ -5,6 +5,7 @@ import { getAllUsers } from './controllers/get-all-users'
 import { getUser } from './controllers/get-user'
 
 export function Routes(app: Express) {
+  /* User routes */
   app.post('/user', registerUser)
   app.get('/users', getAllUsers)
   app.get('/user/:id', getUser)
