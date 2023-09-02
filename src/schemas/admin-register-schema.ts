9815badb793d50cbf9e3c0bc7148dbdfcfbe8d67
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-09-02 17:24:14 -0300

    feat: register admin controller

--- src/schemas/admin-register-schema.ts
+++ src/schemas/admin-register-schema.ts
@@ -28,22 +28,3 @@ export const AdminRegisterSchema = z.object({
     })
     .max(15),
 })
-
-export const AdminAuthenticationSchema = z.object({
-  cpf: z
-    .string({
-      required_error: 'CPF obrigatório!',
-      invalid_type_error: 'Formator de CPF inválido!',
-    })
-    .max(14),
-  password: z
-    .string({
-      required_error: 'Senha obrigatória!',
-      invalid_type_error: 'Senha com formato inválido!',
-    })
-    .max(15),
-})
-
-export const AdminIdSchema = z.object({
-  id: z.string().uuid({ message: 'Id inválido!' }),
-})
